192654e34a717f2f84382ecaaf0eef4284c7a10d
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-07-30 10:19:35 +0200

    Update sce.rb

--- Formula/sce.rb
+++ Formula/sce.rb
@@ -1,8 +1,8 @@
 class Sce < Formula
     desc "SCE - Simple Code Editor"
     homepage "https://github.com/valerioedu/SCE2"
-    url "https://github.com/valerioedu/SCE2/archive/refs/tags/v1.1.0.tar.gz"
-    sha256 "e6568097fc3004bc446655672a54935f3d0e2ec740d41f1b169247eb714c30f4"
+    url "https://github.com/valerioedu/SCE/archive/refs/tags/v1.1.1.tar.gz"
+    sha256 "3e64de688b9e19166277f362c55ad8b3fbe5d084a9c0361414d7943fe530f8c5"
     license "GPL-3.0"
 
     depends_on "cmake" => :build
